33b7fdb2940ec73dadb070db39855dc51d88f967
Massin Guerdi <massinguerdi@web.de>
2023-03-29 01:31:40 +0200

    refactor & elaborate greens, clean up tests

--- jeha_test.thy
+++ jeha_test.thy
@@ -4,48 +4,74 @@ imports Main
 
 begin
 
+(* Antiquotations for term and type patterns from the cookbook. *)
+ML \<open>
+  val term_pat_setup =
+  let
+    val parser = Args.context -- Scan.lift Parse.embedded_inner_syntax
+
+    fun term_pat (ctxt, str) =
+      str |> Proof_Context.read_term_pattern ctxt |> ML_Syntax.print_term |> ML_Syntax.atomic
+  in
+    ML_Antiquotation.inline @{binding "term_pat"} (parser >> term_pat)
+  end
+
+  val type_pat_setup =
+  let
+    val parser = Args.context -- Scan.lift Parse.embedded_inner_syntax
+
+    fun typ_pat (ctxt, str) =
+      let
+        val ctxt' = Proof_Context.set_mode Proof_Context.mode_schematic ctxt
+      in
+        str |> Syntax.read_typ ctxt' |> ML_Syntax.print_typ |> ML_Syntax.atomic
+      end
+  in
+    ML_Antiquotation.inline @{binding "typ_pat"} (parser >> typ_pat)
+  end
+\<close>
+
+setup \<open> term_pat_setup \<close>
+
+setup \<open> type_pat_setup \<close>
+
+
 ML_file jeha.ML
 
 ML \<open>
-  fun pwrite_term ctxt t = Pretty.writeln (Syntax.pretty_term ctxt t)
-  fun pwrite_terms ctxt terms =
+  fun pretty_term ctxt t = Syntax.pretty_term ctxt t |> Pretty.string_of
+  fun pretty_terms ctxt terms =
     terms
     |> map (Syntax.pretty_term ctxt)
-    |> Pretty.commas |> Pretty.block |> Pretty.writeln
+    |> Pretty.commas |> Pretty.block |> Pretty.string_of
 \<close>
 
 ML \<open>
-  val quick_sig = ["a", "b", "f", "g", "h", "p", "q"]
-  fun free_to_const (t as (Free (name, T))) = if exists (curry (op =) name) quick_sig then Const (name, T) else t
-    | free_to_const t = t
-  fun const_to_free (t as (Const (name, T))) = if exists (curry (op =) name) quick_sig then Free (name, T) else t
-    | const_to_free t = t
-  fun pwrite_quicksig ctxt = pwrite_term ctxt o (map_aterms const_to_free);
-  val term_with_subterms = @{term "f (g (\<not> p)) (\<forall> x . q) (y a) (\<lambda> x . h b)"};
-  pwrite_term @{context} term_with_subterms;
-  val tposs = Jeha.green_tposs_of (map_aterms free_to_const term_with_subterms);
-  val green_subterms = map (Jeha.subterm_at_tpos term_with_subterms) tposs;
-  fun pwrite_term_quicksig ctxt = pwrite_term ctxt o (map_aterms const_to_free);
-  fun pwrite_terms_quicksig ctxt = pwrite_terms ctxt o (map (map_aterms const_to_free));
-  writeln "greens:"; map (pwrite_term_quicksig @{context}) green_subterms;
-  val non_green_subterms = Jeha.fold_non_greens cons (map_aterms free_to_const term_with_subterms) [];
-  writeln "nongreens:"; map (pwrite_term_quicksig @{context}) non_green_subterms;
-  (* val unnormed = @{term "\<lambda> y . \<exists> x . g x y (z y) (f x)"}; (* don't rewrite: WORKS *) *)
-  (* Question: Why does a lone quantifier not make sense to isabelle? (can't be parsed using @{term ...} *)
-  val unnormed = Const (@{const_name "HOL.Ex"}, @{typ "('a \<Rightarrow> bool) \<Rightarrow> bool"}) (* rewrite: WORKS *)
-  (* val unnormed = @{term "\<exists> x . x a"}; (* rewrite: WORKS *) *)
-  (* val unnormed = @{term "\<forall> x . f (\<lambda> y . x)"} (* rewrite: WORKS *); *)
-  val normed = map_aterms const_to_free (Jeha.norm_quantifier_poly_eq (map_aterms free_to_const unnormed));
-  writeln "Q\<^sub>\<approx>:"; pwrite_term @{context} unnormed; pwrite_term @{context} normed;
-  fun pwrite_green_non_green_poss ctxt term =
+  @{term_pat ?x}
+\<close>
+
+ML \<open>
+  fun pretty_green_nongreen_subterms ctxt term =
     let
-      val tposs = Jeha.green_tposs_of (map_aterms free_to_const term);
+      val tposs = Jeha.green_tposs_of term;
       val green_subterms = map (Jeha.subterm_at_tpos term) tposs;
-      val non_green_subterms = Jeha.fold_non_greens cons (map_aterms free_to_const term) [];
+      val non_green_subterms = Jeha.fold_non_greens cons term [];
     in
-      writeln "term:"; pwrite_term ctxt term; writeln "greens:"; pwrite_terms_quicksig ctxt green_subterms;
-      writeln "nongreens:"; pwrite_terms_quicksig @{context} non_green_subterms
+      "term:\t\t" ^ pretty_term ctxt term ^ "\ngreens:\t\t" ^ pretty_terms ctxt green_subterms ^
+      "\nnongreens:\t" ^ pretty_terms @{context} non_green_subterms
     end;
-  (* Making Higher-Order Superposition work, Example 5 *)
-  pwrite_green_non_green_poss @{context} @{term "F a \<and> p (\<forall> x . q x) b"}
+  writeln (pretty_green_nongreen_subterms @{context} @{term_pat "f (g (\<not> p)) (\<forall> x . q) (?y a) (\<lambda> x . h b)"});
+  fun pretty_normed_unnormed ctxt term =
+    let val normed = Jeha.norm_quantifier_poly_eq term in
+    "Q\<^sub>\<approx>: " ^ pretty_term ctxt term ^ "  \<mapsto>  " ^ pretty_term ctxt normed end;
+  val no_rewrite_tests = [@{term "\<lambda> y . \<exists> x . g x y (z y) (f x)"}];
+  val rewrite_tests = [@{term "\<exists> x . x a"}, Const (@{const_name "HOL.Ex"}, @{typ "('a \<Rightarrow> bool) \<Rightarrow> bool"}), @{term "\<forall> x . f (\<lambda> y . x)"}];
+  (* Question: Why does a lone quantifier not make sense to isabelle? (can't be parsed using @{term ...}) *)
+  (* writeln "Q\<^sub>\<approx>:"; pwrite_term @{context} unnormed; pwrite_term @{context} normed; *)
+  writeln "shouldn't rewrite:";
+  map (writeln o pretty_normed_unnormed @{context}) no_rewrite_tests;
+  writeln "should rewrite:";
+  map (writeln o pretty_normed_unnormed @{context}) rewrite_tests;
+  writeln "Making Higher-Order Superposition work, Example 5:";
+  writeln (pretty_green_nongreen_subterms @{context} @{term_pat "?F a \<and> p (\<forall> x . q x) b"});
 \<close>
