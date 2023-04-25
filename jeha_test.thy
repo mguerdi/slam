8379a06108014f63953106c3161f8e411268ebfe
Massin Guerdi <massinguerdi@web.de>
2023-04-25 15:55:43 +0200

    dedicated structures for terms, literals, clauses

--- jeha_test.thy
+++ jeha_test.thy
@@ -88,28 +88,28 @@ declare [[ML_exception_trace = true]]
 declare [[ML_exception_debugger = true]]
 
 ML_file_debug \<open>jeha_common.ML\<close>
-ML_file_debug \<open>jeha_lang.ML\<close>
+ML_file_debug \<open>jterm.ML\<close>
+ML_file_debug \<open>jlit.ML\<close>
+ML_file_debug \<open>jclause.ML\<close>
+ML_file_debug \<open>clause_id.ML\<close>
 ML_file_debug \<open>jeha_order.ML\<close>
 ML_file_debug \<open>jeha_subsumption.ML\<close>
 ML_file_debug \<open>jeha.ML\<close>
 ML_file_debug \<open>jeha_tactic.ML\<close>
 
 ML \<open>
-\<close>
-
-ML \<open>
   fun pretty_green_nongreen_subterms ctxt term =
     let
-      val tposs = Jeha_Lang.green_tposs_of term;
-      val green_subterms = map (Jeha_Lang.subterm_at_tpos term) tposs;
-      val non_green_subterms = Jeha_Lang.fold_non_greens cons term [];
+      val tposs = JTerm.green_tposs_of term;
+      val green_subterms = map (JTerm.subterm_at_tpos term) tposs;
+      val non_green_subterms = JTerm.fold_non_greens cons term [];
     in
       "term:\t\t" ^ pretty_term ctxt term ^ "\ngreens:\t\t" ^ pretty_terms ctxt green_subterms ^
       "\nnongreens:\t" ^ pretty_terms @{context} non_green_subterms
     end;
   writeln (pretty_green_nongreen_subterms @{context} @{term_pat "f (g (\<not> p)) (\<forall> x . q) (?y a) (\<lambda> x . h b)"});
   fun pretty_normed_unnormed ctxt term =
-    let val normed = Jeha_Lang.norm_quantifier_poly_eq term in
+    let val normed = JTerm.norm_quantifier_poly_eq term in
     "Q\<^sub>\<approx>: " ^ pretty_term ctxt term ^ "  \<mapsto>  " ^ pretty_term ctxt normed end;
   val no_rewrite_tests = [@{term "\<lambda> y . \<exists> x . g x y (z y) (f x)"}];
   val rewrite_tests = [@{term "\<exists> x . x a"}, Const (@{const_name "HOL.Ex"}, @{typ "('a \<Rightarrow> bool) \<Rightarrow> bool"}), @{term "\<forall> x . f (\<lambda> y . x)"}];
@@ -128,22 +128,22 @@ ML \<open>
 ML \<open>
   (* part of Example 25 from o\<lambda>Sup paper (WORKS) *)
   val (a, b, w) = (@{term "a::'a"}, @{term "b::'a"}, @{term_pat "?w::'a \<Rightarrow> 'a \<Rightarrow> 'a \<Rightarrow> 'a"});
-  val cs = Jeha.infer_sup @{context} ([(w $ a $ b $ b, w $ b $ a $ b, true)], (Jeha_Lang.Left, 0)) ([(a, b, false)], ([], Jeha_Lang.Right, 0));
-  writeln (pretty_terms @{context} (map Jeha_Lang.term_of_clause cs));
+  val cs = Jeha.infer_sup @{context} ({ literals = [(w $ a $ b $ b, w $ b $ a $ b, true)], id = 0 }, (JLit.Left, 0)) ({ literals = [(a, b, false)], id = 1 }, ([], JLit.Right, 0));
+  writeln (pretty_terms @{context} (map JClause.term_of cs));
   (* part of Example 27 from the o\<lambda>Sup paper (WORKS) *)
   val (a, b, h, g) = (@{term "a::'a"}, @{term "b::'a"}, @{term "h::'b \<Rightarrow> 'c"}, @{term "g::bool \<Rightarrow> 'c"})
   val (x2, x3) = (@{term_pat "?x2 :: 'a \<Rightarrow> 'd"}, @{term_pat "?x3 :: 'a \<Rightarrow> 'd"});
   (* val d = [((h $ (g $ (HOLogic.mk_eq (x2 $ a, x3 $ a)))), h $ (g $ @{term True}), false), (@{term False}, @{term True}, true), (x2 $ b, x3 $ b, true)];
   val d_term = Jeha.term_of_clause d; *)
-  val d = Jeha_Lang.clause_of @{term_pat "h(g(?x2 a = ?x3 a)) \<noteq> h(g(True)) \<or> False = True \<or> ?x2 b = ?x3 b"};
+  val d = JClause.of_term (@{term_pat "h(g(?x2 a = ?x3 a)) \<noteq> h(g(True)) \<or> False = True \<or> ?x2 b = ?x3 b"}, 2);
   (* writeln ("D_anti: " ^ pretty_term @{context} d_anti);
-  writeln ("D_anti_clause: " ^ pretty_term @{context} (Jeha_Lang.term_of_clause (Jeha_Lang.clause_of d_anti))); *)
-  writeln ("D: " ^ pretty_term @{context} (Jeha_Lang.term_of_clause d));
-  val c = [(a, b, false)];
-  writeln ("C: " ^ pretty_term @{context} (Jeha_Lang.term_of_clause c));
-  val cs = Jeha.infer_sup @{context} (d, (Jeha_Lang.Left, 2)) (c, ([], Jeha_Lang.Right, 0));
-  val cs_terms = map Jeha_Lang.term_of_clause cs;
-  writeln (pretty_terms (verbose_of @{context}) (map Jeha_Lang.term_of_clause cs));
+  writeln ("D_anti_clause: " ^ pretty_term @{context} (JClause.term_of (JClause.of_term d_anti))); *)
+  writeln ("D: " ^ pretty_term @{context} (JClause.term_of d));
+  val c = { literals = [(a, b, false)], id = 3 };
+  writeln ("C: " ^ pretty_term @{context} (JClause.term_of c));
+  val cs = Jeha.infer_sup @{context} (d, (JLit.Left, 2)) (c, ([], JLit.Right, 0));
+  val cs_terms = map JClause.term_of cs;
+  writeln (pretty_terms (verbose_of @{context}) (map JClause.term_of cs));
 \<close>
 
 declare [[jeha_trace = true]]
@@ -175,6 +175,7 @@ apply(tactic \<open>assume_tac @{context} 1\<close>)
 *)
 *)
 
+(* 
 lemma test:
   shows "(x = y) \<Longrightarrow> (y = z) \<Longrightarrow> (x = z)"
   by jeha
@@ -238,6 +239,139 @@ next
     by (jeha n_leaves.simps(2) add.commute reflect.simps(2))
 qed
 
+(* 
+ML \<open>
+  val dumped_state = !Jeha_Tactic.dump;
+  val dumped_states = length dumped_state;
+  val (ctxt, countdown, passive, active) = nth dumped_state 0
+  val _ = Jeha.given_clause_loop false (Jeha_Common.verbose_of ctxt) countdown passive active (* handle TYPE (msg, _, ts) => error (msg ^ " " ^ (Jeha_Common.pretty_terms (verbose_of ctxt) ts)) *)
+\<close>
+*)
+
+lemma n_nodes_reflect: "n_nodes (reflect t) = n_nodes t"
+proof (induct t)
+  case Lf thus ?case by (jeha reflect.simps(1))
+next
+  case (Br a t1 t2) thus ?case
+    by (jeha add.commute n_nodes.simps(2) reflect.simps(2))
+qed
+
+lemma depth_reflect: "depth (reflect t) = depth t"
+apply (induct t)
+ apply (jeha depth.simps(1) reflect.simps(1))
+by (jeha depth.simps(2) max.commute reflect.simps(2))
+
+text \<open>
+The famous relationship between the numbers of leaves and nodes.
+\<close>
+
+lemma n_leaves_nodes: "n_leaves t = Suc (n_nodes t)"
+apply (induct t)
+ apply (metis n_leaves.simps(1) n_nodes.simps(1))
+by auto
+
+lemma reflect_reflect_ident: "reflect (reflect t) = t"
+apply (induct t)
+ apply (metis reflect.simps(1))
+proof -
+  fix a :: 'a and t1 :: "'a bt" and t2 :: "'a bt"
+  assume A1: "reflect (reflect t1) = t1"
+  assume A2: "reflect (reflect t2) = t2"
+  have "\<And>V U. reflect (Br U V (reflect t1)) = Br U t1 (reflect V)"
+    using A1 by (metis reflect.simps(2))
+  hence "\<And>V U. Br U t1 (reflect (reflect V)) = reflect (reflect (Br U t1 V))"
+    by (metis reflect.simps(2))
+  hence "\<And>U. reflect (reflect (Br U t1 t2)) = Br U t1 t2"
+    using A2 by metis
+  thus "reflect (reflect (Br a t1 t2)) = Br a t1 t2" by blast
+qed
+
+lemma bt_map_ident: "bt_map (%x. x) = (%y. y)"
+apply (rule ext)
+apply (induct_tac y)
+ apply (metis bt_map.simps(1))
+by (metis bt_map.simps(2))
+
+lemma bt_map_append: "bt_map f (append t u) = append (bt_map f t) (bt_map f u)"
+apply (induct t)
+ apply (metis append.simps(1) bt_map.simps(1))
+by (metis append.simps(2) bt_map.simps(2))
+
+lemma bt_map_compose: "bt_map (f o g) t = bt_map f (bt_map g t)"
+apply (induct t)
+ apply (metis bt_map.simps(1))
+by (metis bt_map.simps(2) o_eq_dest_lhs)
+
+lemma bt_map_reflect: "bt_map f (reflect t) = reflect (bt_map f t)"
+apply (induct t)
+ apply (metis bt_map.simps(1) reflect.simps(1))
+by (metis bt_map.simps(2) reflect.simps(2))
+
+lemma preorder_bt_map: "preorder (bt_map f t) = map f (preorder t)"
+apply (induct t)
+ apply (metis bt_map.simps(1) list.map(1) preorder.simps(1))
+by simp
+
+lemma inorder_bt_map: "inorder (bt_map f t) = map f (inorder t)"
+proof (induct t)
+  case Lf thus ?case
+  proof -
+    have "map f [] = []" by (metis list.map(1))
+    hence "map f [] = inorder Lf" by (metis inorder.simps(1))
+    hence "inorder (bt_map f Lf) = map f []" by (metis bt_map.simps(1))
+    thus "inorder (bt_map f Lf) = map f (inorder Lf)" by (metis inorder.simps(1))
+  qed
+next
+  case (Br a t1 t2) thus ?case by simp
+qed
+
+lemma postorder_bt_map: "postorder (bt_map f t) = map f (postorder t)"
+apply (induct t)
+ apply (metis Nil_is_map_conv bt_map.simps(1) postorder.simps(1))
+by simp
+
+lemma depth_bt_map [simp]: "depth (bt_map f t) = depth t"
+apply (induct t)
+ apply (metis bt_map.simps(1) depth.simps(1))
+by simp
+
+lemma n_leaves_bt_map [simp]: "n_leaves (bt_map f t) = n_leaves t"
+apply (induct t)
+ apply (metis bt_map.simps(1) n_leaves.simps(1))
+proof -
+  fix a :: 'b and t1 :: "'b bt" and t2 :: "'b bt"
+  assume A1: "n_leaves (bt_map f t1) = n_leaves t1"
+  assume A2: "n_leaves (bt_map f t2) = n_leaves t2"
+  have "\<And>V U. n_leaves (Br U (bt_map f t1) V) = n_leaves t1 + n_leaves V"
+    using A1 by (metis n_leaves.simps(2))
+  hence "\<And>V U. n_leaves (bt_map f (Br U t1 V)) = n_leaves t1 + n_leaves (bt_map f V)"
+    by (metis bt_map.simps(2))
+  hence F1: "\<And>U. n_leaves (bt_map f (Br U t1 t2)) = n_leaves t1 + n_leaves t2"
+    using A2 by metis
+  have "n_leaves t1 + n_leaves t2 = n_leaves (Br a t1 t2)"
+    by (metis n_leaves.simps(2))
+  thus "n_leaves (bt_map f (Br a t1 t2)) = n_leaves (Br a t1 t2)"
+    using F1 by metis
+qed
+
+lemma preorder_reflect: "preorder (reflect t) = rev (postorder t)"
+apply (induct t)
+ apply (metis Nil_is_rev_conv postorder.simps(1) preorder.simps(1)
+              reflect.simps(1))
+apply simp
+done
+
+lemma inorder_reflect: "inorder (reflect t) = rev (inorder t)"
+apply (induct t)
+ apply (metis Nil_is_rev_conv inorder.simps(1) reflect.simps(1))
+by simp
+(* Slow:
+by (metis append.simps(1) append_eq_append_conv2 inorder.simps(2)
+          reflect.simps(2) rev.simps(2) rev_append)
+*)
+
+*)
+
 (*
 ML \<open>
   val dumped_state = !Jeha_Tactic.dump;
@@ -262,8 +396,8 @@ ML \<open>
   (* val eq_trans = Thm.prop_of @{thm test}
   val (axioms, conjecture) = Logic.strip_prems (2, [], eq_trans)
   val clauses = HOLogic.mk_not (Object_Logic.atomize_term @{context} conjecture) :: map (Object_Logic.atomize_term @{context}) axioms *)
-  val clauses = map Jeha_Lang.clause_of [@{term "x = y"}, @{term "y = z"}, @{term "x \<noteq> z"}]
-  val _ = writeln (Jeha_Common.pretty_terms (Jeha_Common.verbose_of @{context}) (map Jeha_Lang.term_of_clause clauses))
+  val clauses = map_index (fn (i, t) => JClause.of_term (t, i)) [@{term "x = y"}, @{term "y = z"}, @{term "x \<noteq> z"}]
+  val _ = writeln (Jeha_Common.pretty_terms (Jeha_Common.verbose_of @{context}) (map JClause.term_of clauses))
   val result = Jeha.given_clause_loop false @{context} 12 (Jeha.passive_set_of_list clauses) []
 \<close>
 
@@ -271,8 +405,8 @@ ML \<open>
   val f = @{term "f :: 'a \<Rightarrow> 'a"}
   val var_x = Var (("x", 0), @{typ "'a"})
   val f_is_id = HOLogic.mk_eq (f $ var_x, var_x)
-  val clauses = map Jeha_Lang.clause_of [f_is_id, @{term "f (f y) \<noteq> y"}]
-  val _ = writeln (Jeha_Common.pretty_terms (Jeha_Common.verbose_of @{context}) (map Jeha_Lang.term_of_clause clauses))
+  val clauses = map_index (fn (i, t) => JClause.of_term (t, i)) [f_is_id, @{term "f (f y) \<noteq> y"}]
+  val _ = writeln (Jeha_Common.pretty_terms (Jeha_Common.verbose_of @{context}) (map JClause.term_of clauses))
   val result = Jeha.given_clause_loop false @{context} 5 (Jeha.passive_set_of_list clauses) []
 \<close>
 
@@ -285,8 +419,8 @@ ML \<open>
   val zero_right_neutral = HOLogic.mk_eq (plus $ var_x $ zero, var_x)
   val plus_def = HOLogic.mk_eq (plus $ var_x $ (s $ var_y), s $ (plus $ var_x $ var_y))
   val two_neq_one_plus_one = @{term "s (s z) \<noteq> p (s z) (s z)"}
-  val clauses = map Jeha_Lang.clause_of [zero_right_neutral, plus_def, two_neq_one_plus_one]
-  val _ = writeln (Jeha_Common.pretty_terms (Jeha_Common.verbose_of @{context}) (map Jeha_Lang.term_of_clause clauses))
+  val clauses = map_index (fn (i, t) => JClause.of_term (t, i)) [zero_right_neutral, plus_def, two_neq_one_plus_one]
+  val _ = writeln (Jeha_Common.pretty_terms (Jeha_Common.verbose_of @{context}) (map JClause.term_of clauses))
   (* FIXME: looks like some rewriting steps are missing pretty early on *)
   val result = Jeha.given_clause_loop false @{context} 12 (Jeha.passive_set_of_list clauses) []
 \<close>
@@ -300,19 +434,21 @@ ML \<open>
   val eq = HOLogic.mk_eq (t $ (t $ var_x $ var_y) $ (t $ var_y $ var_z), var_y)
   (* one of the rules in the completed rewrite system *)
   val xxyz_neq_xy = HOLogic.mk_not (HOLogic.mk_eq (t $ var_x $ (t $ (t $ var_x $ var_y) $ var_z), t $ var_x $ var_y))
-  val clauses = map Jeha_Lang.clause_of [eq, xxyz_neq_xy]
-  val _ = writeln (Jeha_Common.pretty_terms (Jeha_Common.verbose_of @{context}) (map Jeha_Lang.term_of_clause clauses))
+  val clauses = map_index (fn (i, t) => JClause.of_term (t, i)) [eq, xxyz_neq_xy]
+  val _ = writeln (Jeha_Common.pretty_terms (Jeha_Common.verbose_of @{context}) (map JClause.term_of clauses))
   val result = Jeha.given_clause_loop false @{context} 5 (Jeha.passive_set_of_list clauses) []
 \<close>
 
+(*
 ML \<open>
   val c1 = @{term "P a :: bool"}
   val c2 = @{term "a = b"}
   val c3 = @{term "\<not> P b"}
-  val clauses = map Jeha_Lang.clause_of [c1, c2, c3]
-  val _ = writeln (Jeha_Common.pretty_terms (Jeha_Common.verbose_of @{context}) (map Jeha_Lang.term_of_clause clauses))
+  val clauses = map JClause.of_term [c1, c2, c3]
+  val _ = writeln (Jeha_Common.pretty_terms (Jeha_Common.verbose_of @{context}) (map JClause.term_of clauses))
   val result = Jeha.given_clause_loop false @{context} 10 (Jeha.passive_set_of_list clauses) []
 \<close>
+*)
 
 ML \<open>
   val x_ord_x = Jeha_Order.kbo (@{term_pat "?x"}, @{term_pat "?x"})
@@ -336,4 +472,5 @@ begin
 
   term ?t
 end
+
 *)
