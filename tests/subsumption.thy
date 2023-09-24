045ec8b4645d250699f3a02810429f58a8fdf131
Massin Guerdi <massinguerdi@web.de>
2023-09-24 14:48:02 +0200

    unit tests for subsumption and superposition

---
+++ tests/subsumption.thy
@@ -0,0 +1,74 @@
+theory subsumption
+
+imports HOL.HOL
+
+begin
+
+ML_file \<open>../jeha_common.ML\<close>
+ML_file \<open>../clause_id.ML\<close>
+ML_file \<open>../jterm.ML\<close>
+ML_file \<open>../jeha_order_reference.ML\<close>
+ML_file \<open>../jeha_order.ML\<close>
+ML_file \<open>../jlit.ML\<close>
+ML_file \<open>../jclause_pos.ML\<close>
+ML_file \<open>../jeha_log.ML\<close>
+ML_file \<open>../jclause.ML\<close>
+(* ML_file \<open>jeha_proof.ML\<close> *)
+ML_file \<open>../jeha_unify.ML\<close>
+ML_file \<open>../jeha_subsumption.ML\<close>
+ML_file \<open>../jeha.ML\<close>
+ML_file \<open>../jeha_tactic.ML\<close>
+
+declare [[jeha_trace]]
+
+setup \<open>Jeha_Common.term_pat_setup\<close>
+setup \<open>Jeha_Common.type_pat_setup\<close>
+
+(* no unification *)
+ML_val \<open>
+  val subset = JClause.of_term (@{term_pat "x = y"}, 0);
+  val superset = JClause.of_term (@{term_pat "x = y \<or> v = w"}, 1);
+  \<^assert> (Jeha_Subsumption.subsumes (Context.Proof @{context}) (subset, superset))
+\<close>
+
+(* multiset subsumption *)
+ML_val \<open>
+  val not_sub_multiset = JClause.of_term (@{term_pat "x = y \<or> x = y"}, 0);
+  val superset = JClause.of_term (@{term_pat "x = y \<or> v = w"}, 1);
+  \<^assert> (not
+    (Jeha_Subsumption.subsumes (Context.Proof @{context}) (not_sub_multiset, superset))
+  )
+\<close>
+
+(* simple unification *)
+ML_val \<open>
+  val sub_generalization = JClause.of_term (@{term_pat "f ?x = g ?x"}, 0);
+  val superset = JClause.of_term (@{term_pat "f c = g c \<or> False"}, 1);
+  \<^assert> (Jeha_Subsumption.subsumes (Context.Proof @{context}) (sub_generalization, superset))
+\<close>
+
+(* unordered literals *)
+ML_val \<open>
+  val unit = JClause.of_term (@{term_pat "c = d"}, 0);
+  val flipped_unit = JClause.of_term (@{term_pat "d = c"}, 1);
+  \<^assert> (Jeha_Subsumption.subsumes (Context.Proof @{context}) (unit, flipped_unit))
+\<close>
+
+(* backtracking *)
+ML_val \<open>
+  (* first attempts ?y := c but only ?y := d works *)
+  val c = JClause.of_term (@{term_pat "c = ?y \<or> c = c"}, 0);
+  val d = JClause.of_term (@{term_pat "c = c \<or> False \<or> d = c"}, 1);
+  \<^assert> (Jeha_Subsumption.subsumes (Context.Proof @{context}) (c, d))
+\<close>
+
+(* no matcher *)
+ML_val \<open>
+  val c = JClause.of_term (@{term_pat "c = ?y \<or> d = ?y"}, 0);
+  val d = JClause.of_term (@{term_pat "c = e \<or> d = f"}, 1);
+  \<^assert> (not
+    (Jeha_Subsumption.subsumes (Context.Proof @{context}) (c, d))
+  )
+\<close>
+
+end
---
+++ tests/sup.thy
@@ -0,0 +1,91 @@
+theory sup
+
+imports HOL.HOL
+
+begin
+
+ML_file \<open>../jeha_common.ML\<close>
+ML_file \<open>../clause_id.ML\<close>
+ML_file \<open>../jterm.ML\<close>
+ML_file \<open>../jeha_order_reference.ML\<close>
+ML_file \<open>../jeha_order.ML\<close>
+ML_file \<open>../jlit.ML\<close>
+ML_file \<open>../jclause_pos.ML\<close>
+ML_file \<open>../jeha_log.ML\<close>
+ML_file \<open>../jclause.ML\<close>
+(* ML_file \<open>jeha_proof.ML\<close> *)
+ML_file \<open>../jeha_unify.ML\<close>
+ML_file \<open>../jeha_subsumption.ML\<close>
+ML_file \<open>../jeha.ML\<close>
+ML_file \<open>../jeha_tactic.ML\<close>
+
+declare [[jeha_trace]]
+
+declare [[jeha_disable_all]]
+declare [[jeha_rule_sup]]
+declare [[jeha_rule_e_res]]
+
+(* positive test cases *)
+
+lemma transitivity:
+  shows "x = y \<Longrightarrow> y = z \<Longrightarrow> x = z"
+  by (jeha)
+
+lemma congruence:
+  shows "x = y \<Longrightarrow> f x = f y"
+  by (jeha)
+
+lemma deep_congruence:
+  shows "x = y \<Longrightarrow> v = w  \<Longrightarrow> f (g (h x) w) = f (g (h y) v)"
+  by (jeha)
+
+(* negative test cases *)
+
+lemma no_sup_below_lambda:
+  shows "x = y \<Longrightarrow> (\<lambda> z. x) = (\<lambda> z. y)"
+  by (jeha)
+
+lemma no_sup_below_forall:
+  shows "x = y \<Longrightarrow> (\<forall> z. p x = p y)"
+  by (jeha)
+
+lemma no_sup_in_head:
+  shows "f = g \<Longrightarrow> f x = g x"
+  by (jeha)
+
+
+ML \<open>
+  val c = JClause.of_term (@{term "x \<noteq> z"}, ~1)
+  val x_eligible = JClause.is_eligible_full_pos c ([], JLit.Left, 0)
+  val z_eligible = JClause.is_eligible_full_pos c ([], JLit.Right, 0)
+\<close>
+
+(*
+Sup: 4671:y = z (Axiom) into 4672:x \<noteq> z (Axiom) 
+Sup: z ?= z 
+Sup: u eligible=true 
+Sup: tt' eligible=true 
+c_comp_d SOME LESS 
+t_comp_t' SOME GREATER 
+
+\<not>(C\<sigma> \<lesssim> D\<sigma>) \<Longleftrightarrow> \<not>(x \<noteq> z \<lesssim> y = z)
+
+{{x, \<bottom>}, {z, \<bottom>}} < {{y}, {z}}
+
+with \<bottom> < x < y < z
+
+*)
+
+ML \<open>
+  val ms_ms_int_ord = Jeha_Order.mk_multiset_order_of_strict (Jeha_Order.mk_multiset_order_of_strict (SOME o int_ord))
+  val (l, r) = ([[1, 0], [3, 0]], [[2], [3]])
+  val b = ms_ms_int_ord (l, r)
+  val int_list_eq = Jeha_Order.multiset_eq op=
+  val int_list_g = Jeha_Order.multiset_is_greater_reference op= op>
+  val int_list_list_g = Jeha_Order.multiset_is_greater_reference int_list_g int_list_eq
+  val b' = int_list_list_g (l, r)
+\<close>
+
+
+
+end
