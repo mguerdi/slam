807e97787ccca65349c86f7da7426f4b3faed80d
Massin Guerdi <massinguerdi@web.de>
2023-10-01 21:36:03 +0200

    use jeha.thy instead of ML_file

--- tests/subsumption.thy
+++ tests/subsumption.thy
@@ -1,23 +1,8 @@
 theory subsumption
 
-imports HOL.HOL
+imports "../jeha" HOL.HOL
 
 begin
-
-ML_file \<open>../jeha_common.ML\<close>
-ML_file \<open>../clause_id.ML\<close>
-ML_file \<open>../jterm.ML\<close>
-ML_file \<open>../jeha_order_reference.ML\<close>
-ML_file \<open>../jeha_order.ML\<close>
-ML_file \<open>../jlit.ML\<close>
-ML_file \<open>../jclause_pos.ML\<close>
-ML_file \<open>../jeha_log.ML\<close>
-ML_file \<open>../jclause.ML\<close>
-(* ML_file \<open>jeha_proof.ML\<close> *)
-ML_file \<open>../jeha_unify.ML\<close>
-ML_file \<open>../jeha_subsumption.ML\<close>
-ML_file \<open>../jeha.ML\<close>
-ML_file \<open>../jeha_tactic.ML\<close>
 
 declare [[jeha_trace]]
 
