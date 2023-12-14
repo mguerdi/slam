7b72c8ea4ebac371ab0be0a52abaa91f16e21b3b
Massin Guerdi <massinguerdi@web.de>
2023-12-14 16:49:26 +0100

    clean up boolean_ho test

--- tests/boolean_ho.thy
+++ tests/boolean_ho.thy
@@ -5,46 +5,17 @@ imports "test_base"
 begin
 
 ML_val \<open>
-  val c = JClause.of_term (@{term_pat "(\<forall> x. ?y x = (p x \<and> q x)) = False"}, 0)
-  val subterm = JClause.subterm_at_full_pos c ([], JLit.Left, 0)
-  val c' = Jeha.infer_forall_rw @{context} c ([], JLit.Left, 0)
+  val c = JClause.of_term (@{term_schem "(\<forall> x. ?y x = (p x \<and> q x)) = False"}, 0);
+  val forall_subterm_position = ([], JLit.Left, 0);
+  val forall_subterm = JClause.subterm_at_full_pos c forall_subterm_position;
+  val [conclusion] = Jeha.infer_forall_rw @{context} c forall_subterm_position; 
+  val instead_of_forall = JClause.subterm_at_full_pos conclusion forall_subterm_position;
+  val skolem = @{term_schem "SOME z. \<not> (?y z = (p z \<and> q z))"};
+  val lambda = JTerm.subterm_at forall_subterm [1];
+  val expected = JTerm.norm_beta_eta_qeta (lambda $ skolem);
+  (* val () = writeln (Jeha_Common.pretty_term @{context} expected);
+  val () = writeln (Jeha_Common.pretty_term @{context} instead_of_forall); *)
+  \<^assert> (expected aconv instead_of_forall);
 \<close>
-
-declare [[jeha_trace = true]]
-
-ML \<open>
-  val sb = Config.get @{context} Unify.search_bound
-  val tb = Config.get @{context} Unify.trace_bound
-\<close>
-
-declare [[unify_search_bound = 10]]
-declare [[unify_trace_bound = 10]]
-
-ML \<open>
-  val sb = Config.get @{context} Unify.search_bound
-  val tb = Config.get @{context} Unify.trace_bound
-\<close>
-
-(* declare [[ jeha_disable_all ]] *)
-declare [[ jeha_rule_forall_rw ]]
-
-ML_val \<open>
-  val c = JClause.of_term (@{term_pat "(\<forall> x. ?y x = (p x \<and> q x)) = False"}, 0)
-  val subterm = JClause.subterm_at_full_pos c ([], JLit.Left, 0)
-  val c' = Jeha.infer_forall_rw @{context} c ([], JLit.Left, 0)
-\<close>
-
-ML_val \<open>
-  val all_disabled = Config.get @{context} Jeha_Common.disable_all
-  val sup_enabeld = Config.get @{context} Jeha_Common.rule_sup
-\<close>
-
-lemma fa_rw_test:
-  shows "\<And>y. (\<forall> x. y x = (p x \<and> q x))"
-  (* ML_val \<open>
-    val g = @{Isar.goal}
-    val atomized_ths = map (Conv.fconv_rule (Object_Logic.atomize @{context})) [#goal g]
-  \<close> *)
-  by (jeha)
 
 end
