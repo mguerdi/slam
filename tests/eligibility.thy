2662af291e300f06ac35f83badf616259b12b8da
Massin Guerdi <massinguerdi@web.de>
2023-12-14 17:56:41 +0100

    clean up and extend eligiblity test

--- tests/eligibility.thy
+++ tests/eligibility.thy
@@ -4,21 +4,68 @@ imports "test_base"
 
 begin
 
-(* FIXME: this is from debugging example 26 long ago, but since then the kbo has been fixed. *)
+(* old KBO bug *)
 ML_val \<open>
-  val c = JClause.of_term (@{term_pat "(\<forall>x. ?x x = (p x \<and> q x)) = False \<or> True = False"}, 0)
-  (* both ineligible: *)
-  val left_eligible = JClause.is_eligible_cpos c 0
-  val right_eligible = JClause.is_eligible_cpos c 1
-  val lits = #literals c
-  (* left is maximal *)
-  val is_maximal_left = Jeha_Order.is_maximal JLit.kbo false (nth lits 0) lits
-  val is_maximal_right = Jeha_Order.is_maximal JLit.kbo false (nth lits 1) lits
-  (* but not strict maximal ?? *)
-  val is_strict_maximal_left = Jeha_Order.is_maximal JLit.kbo true (nth lits 0) lits
-  val fa_literal = nth lits 0
-  val lit_kbo_with_itself = JLit.kbo (fa_literal, fa_literal)
-  val is_strict_maximal_in_singleton = Jeha_Order.is_maximal JLit.kbo true fa_literal [fa_literal, nth lits 1]
+  val c = JClause.of_term (@{term_pat "(\<forall>x. ?x x = (p x \<and> q x)) = False \<or> True = False"}, 0);
+
+  val left_eligible = JClause.is_eligible_cpos c 0;
+  \<^assert> (left_eligible);
+
+  val right_eligible = JClause.is_eligible_cpos c 1;
+  \<^assert> (not right_eligible);
+
+  val lits = #literals c;
+
+  val is_maximal_left = Jeha_Order.is_maximal JLit.kbo false (nth lits 0) lits;
+  \<^assert> is_maximal_left;
+
+  val is_maximal_right = Jeha_Order.is_maximal JLit.kbo false (nth lits 1) lits;
+  \<^assert> (not is_maximal_right);
+
+  val is_strict_maximal_left = Jeha_Order.is_maximal JLit.kbo true (nth lits 0) lits;
+  \<^assert> is_strict_maximal_left;
+
+  val lit_kbo_with_itself = JLit.kbo (nth lits 0, nth lits 0);
+  \<^assert> (SOME EQUAL = lit_kbo_with_itself);
+
+  val is_strict_maximal_in_singleton =
+    Jeha_Order.is_maximal JLit.kbo true (nth lits 0) [nth lits 0, nth lits 1];
+  \<^assert> is_strict_maximal_in_singleton;
+\<close>
+
+(* eligible: direct subterms of fully applied equality *)
+ML_val \<open>
+  val t = @{term "x = y"};
+  (* x \<not>\<le> y alphabetically but y < x *)
+  \<^assert> (JClause.is_eligible_tpos t [2]);
+  \<^assert> (not (JClause.is_eligible_tpos t [1]));
+\<close>
+
+(* eligible: direct subterms of fully applied inequality *)
+ML_val \<open>
+  val t = @{term "x \<noteq> y"};
+  (* x \<not>\<le> y alphabetically but y < x *)
+  \<^assert> (JClause.is_eligible_tpos t [1, 2]);
+  \<^assert> (not (JClause.is_eligible_tpos t [1, 1]));
+\<close>
+
+(* not eligbile: subterms of partially applied equality *)
+ML_val \<open>
+  val t = @{term "(=) x"};
+  \<^assert> (not (JClause.is_eligible_tpos t [1]));
+\<close>
+
+(* not eligbile: subterms of partially applied inequality *)
+(* Problem: There is no such thing as a partially applied inequality in Isabelle.
+  Possibilities
+  * (\<not> \<circ> =) x
+  * \<lambda>x. \<not> (x = y) but this would never be eligible because it has no green subterms *)
+
+(* eligible: equality in fully applied inequality *)
+(* This recovers the s \<noteq> t condition of (E4) via an application of (E3) with head (\<not>). *)
+ML_val \<open>
+  val t = @{term "x \<noteq> y"};
+  \<^assert> (JClause.is_eligible_tpos t [1]);
 \<close>
 
 end
