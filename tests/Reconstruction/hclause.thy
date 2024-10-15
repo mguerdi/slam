8625e958f39114b5d56d7f34143861c973e73a1a
Massin Guerdi <massin.guerdi@ifi.lmu.de>
2024-10-15 11:08:47 +0200

    additional HClause tests

--- tests/Reconstruction/hclause.thy
+++ tests/Reconstruction/hclause.thy
@@ -33,13 +33,28 @@ ML_val \<open>
   val () = \<^assert> (Thm.eq_thm (move_from_to 2 2 C, C))
   (* HClause.negate_head *)
   val () = \<^assert> (Thm.eq_thm (negate_head @{context} C, mk @{prop "A \<Longrightarrow> B \<Longrightarrow> \<not>C"}))
+  val () = \<^assert> (Thm.eq_thm (negate_head @{context} (mk @{prop "A \<Longrightarrow> False"}), mk @{prop "\<not>A"}))
   val () = \<^assert> (Thm.eq_thm (negate_head @{context} (mk @{prop "\<not>A \<Longrightarrow> False"}), mk @{prop "A"}))
   val () = \<^assert> (Thm.eq_thm (negate_head @{context} (mk @{prop "\<not>\<not>A \<Longrightarrow> False"}), mk @{prop "\<not>A"}))
   val () = \<^assert_cant>\<open>negate_head @{context} (mk @{prop "A"})\<close>
   val () = \<^assert_cant>\<open>negate_head @{context} (mk @{prop "A \<Longrightarrow> B"})\<close>
 \<close>
 
-ML \<open>
+ML_val \<open>
+  val C = mk @{prop "A \<Longrightarrow> False"}
+  (* HClause.make_last *)
+  val () = \<^assert> (Thm.eq_thm (make_last 0 C, C)) 
+  val () = \<^assert_cant>\<open>HClause.make_last 1 C\<close>
+  (* HClause.move_last_to *)
+  val () = \<^assert> (Thm.eq_thm (move_last_to 0 C, C))
+  val () = \<^assert_cant>\<open>HClause.move_last_to 1 C\<close>
+  (* HClause.move_from_to *)
+  val () = \<^assert> (Thm.eq_thm (move_from_to 0 0 C, C)) 
+  val () = \<^assert_cant>\<open>move_from_to 0 1 C\<close>
+  val () = \<^assert_cant>\<open>move_from_to 1 0 C\<close>
+\<close>
+
+ML_val \<open>
   (* HClause.dest_lit_at *)
   val C = mk @{prop "s = t \<Longrightarrow> u \<noteq> v \<Longrightarrow> False"}
   val (s, t, false) = dest_lit_at 0 C
