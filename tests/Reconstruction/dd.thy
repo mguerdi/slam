9180e64f1a02ae1fa14e5571f7612680c0026329
Massin Guerdi <massin.guerdi@ifi.lmu.de>
2024-11-03 01:18:36 +0100

    fix delete duplicated lits reconstruction

--- tests/Reconstruction/dd.thy
+++ tests/Reconstruction/dd.thy
@@ -4,8 +4,13 @@ imports "JEHA_TEST_BASE.test_base"
 
 begin
 
-ML \<open>
-  val mk = Skip_Proof.make_thm @{theory}
+ML_val \<open>
+  val C = mk @{prop "A \<Longrightarrow> A \<Longrightarrow> B \<Longrightarrow> False"}
+  val expected = mk @{prop "A \<Longrightarrow> B \<Longrightarrow> False"}
+  val conclusion =
+    Jeha_Proof.reconstruct_delete_duplicated_lits
+      { premise = C, duplicate_cposs = [{duplicate_of = 0, duplicate = 1}] }
+  val () = \<^assert> (Thm.eq_thm_prop (expected, conclusion))
 \<close>
 
 ML_val \<open>
@@ -22,7 +27,7 @@ ML_val \<open>
   val expected = mk @{prop "\<not>C' \<Longrightarrow> A \<Longrightarrow> B \<Longrightarrow> C \<Longrightarrow> False"}
   val conclusion =
     Jeha_Proof.reconstruct_delete_duplicated_lits
-      { premise = C, duplicate_cposs = [{duplicate_of = 1, duplicate = 2}] }
+      { premise = C, duplicate_cposs = [{duplicate_of = 1, duplicate = 3}] }
   val () = \<^assert> (Thm.eq_thm_prop (expected, conclusion))
 \<close>
 
@@ -35,6 +40,42 @@ ML_val \<open>
       , duplicate_cposs =
         [ { duplicate_of = 1, duplicate = 3 }
         , { duplicate_of = 2, duplicate = 4 } ] }
+  val () = \<^assert> (Thm.eq_thm_prop (expected, conclusion))
+\<close>
+
+ML_val \<open>
+  val C = mk @{prop "\<not>C' \<Longrightarrow> A \<Longrightarrow> B \<Longrightarrow> B \<Longrightarrow> A \<Longrightarrow> False"}
+  val expected = mk @{prop "\<not>C' \<Longrightarrow> A \<Longrightarrow> B \<Longrightarrow> False"}
+  val conclusion =
+    Jeha_Proof.reconstruct_delete_duplicated_lits
+      { premise = C
+      , duplicate_cposs =
+        [ { duplicate_of = 1, duplicate = 4 }
+        , { duplicate_of = 2, duplicate = 3 } ] }
+  val () = \<^assert> (Thm.eq_thm_prop (expected, conclusion))
+\<close>
+
+ML_val \<open>
+  val C = mk @{prop "\<not>C' \<Longrightarrow> A \<Longrightarrow> B \<Longrightarrow> B \<Longrightarrow> A \<Longrightarrow> False"}
+  val expected = mk @{prop "\<not>C' \<Longrightarrow> A \<Longrightarrow> B \<Longrightarrow> False"}
+  val conclusion =
+    Jeha_Proof.reconstruct_delete_duplicated_lits
+      { premise = C
+      , duplicate_cposs =
+        [ { duplicate_of = 2, duplicate = 3 }
+        , { duplicate_of = 1, duplicate = 4 } ] }
+  val () = \<^assert> (Thm.eq_thm_prop (expected, conclusion))
+\<close>
+
+ML_val \<open>
+  val C = mk @{prop "\<not>C' \<Longrightarrow> A \<Longrightarrow> A \<Longrightarrow> B \<Longrightarrow> B \<Longrightarrow> False"}
+  val expected = mk @{prop "\<not>C' \<Longrightarrow> A \<Longrightarrow> B \<Longrightarrow> False"}
+  val conclusion =
+    Jeha_Proof.reconstruct_delete_duplicated_lits
+      { premise = C
+      , duplicate_cposs =
+        [ { duplicate_of = 3, duplicate = 4 }
+        , { duplicate_of = 1, duplicate = 2 } ] }
   val () = \<^assert> (Thm.eq_thm_prop (expected, conclusion))
 \<close>
 
