a423d75e11dedafa681e6e766027aca0caf40e91
Massin Guerdi <massin.guerdi@ifi.lmu.de>
2024-11-22 16:45:38 +0100

    remove show_types, which breaks HOL-Library.Pattern_Aliases

--- jeha_lemma.thy
+++ jeha_lemma.thy
@@ -4,8 +4,6 @@ imports jeha_base (* HOL.Sledgehammer *) HOL.Hilbert_Choice
 
 begin
 
-declare [[show_types]]
-
 ML \<open>
   (* Goal: Only instantiate the term variables. Instantiate type variables as required by the term
   instantiation.
