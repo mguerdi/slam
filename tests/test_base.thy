e5145cdd74fc119540bbdf75e1d7f71df6cc51e8
Massin Guerdi <massin.guerdi@ifi.lmu.de>
2025-01-12 13:29:42 +0100

    fix assert_cant antiquotation (broken on isabelle-dev)

--- tests/test_base.thy
+++ tests/test_base.thy
@@ -12,9 +12,23 @@ setup \<open>Jeha_Common.term_schem_setup\<close>
 setup \<open>Jeha_Common.type_pat_setup\<close>
 
 ML \<open>
-  fun cant f x = (not oo Basics.can) f x
-  fun report cant = if cant then () else raise General.Fail "Did not raise exception"
-  val _ = Theory.setup (ML_Antiquotation.special_form \<^binding>\<open>assert_cant\<close> "() |> (report oo cant)")
+  fun cant f = (not o Isabelle_Thread.can) f
+  fun unit_cant f = if cant f then () else raise General.Fail "Did not raise exception"
+
+  local
+    val tokenize = ML_Lex.tokenize_no_range;
+    val tokenize_range = ML_Lex.tokenize_range o Input.range_of;
+    val tokenize_text = map Antiquote.Text o ML_Lex.tokenize_no_range;
+
+    val bg_expr = tokenize_text "(fn () =>";
+    val en_expr = tokenize_text ")";
+    fun make_expr a = bg_expr @ a @ en_expr;
+
+    fun parse_cant (_: Proof.context) input =
+    ("unit_cant", [make_expr (ML_Lex.read_source input)]);
+  in
+    val _ = Theory.setup (ML_Antiquotation.special_form \<^binding>\<open>assert_cant\<close> parse_cant)
+  end
 \<close>
 
 ML \<open>
