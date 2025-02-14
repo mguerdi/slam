c8415a7585d1c70923a344b2d36c78178958c526
Massin Guerdi <massin.guerdi@ifi.lmu.de>
2025-02-14 20:19:24 +0100

    fix patching of jeha into isabelle

--- patches/Isabelle/src/HOL/jeha_base.thy
+++ patches/Isabelle/src/HOL/jeha_base.thy
@@ -12,6 +12,7 @@ ML_file \<open>Tools/Jeha/jclause_pos.ML\<close>
 ML_file \<open>Tools/Jeha/jeha_log.ML\<close>
 ML_file \<open>Tools/Jeha/jclause.ML\<close>
 ML_file \<open>Tools/Jeha/jeha_argo.ML\<close>
+ML_file \<open>Tools/Jeha/jeha_index.ML\<close>
 ML_file \<open>Tools/Jeha/jeha_unify.ML\<close>
 ML_file \<open>Tools/Jeha/jeha_subsumption.ML\<close>
 ML_file \<open>Tools/Jeha/jeha_simplify.ML\<close>
