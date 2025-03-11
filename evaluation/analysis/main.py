3cb86541ee93becd0d0a5530ab7a4d8a03dca330
Massin Guerdi <massin.guerdi@ifi.lmu.de>
2025-03-11 16:03:39 +0100

    evaluation: improve analysis script

--- evaluation/analysis/main.py
+++ evaluation/analysis/main.py
@@ -42,10 +42,16 @@ def analyse_file(filename):
     failed = [goal for goal in results if is_failed(goal)]
     timed_out = [goal for goal in results if is_timeout(goal)]
     success = [goal for goal in results if is_success(goal)]
-    print(f"{len(failed)} failed")
-    print(f"{len(timed_out)} timed out")
-    print(f"{len(success)} succeeded")
+    print(f"{len(failed)} calls failed")
+    print(f"{len(timed_out)} calls timed out")
+    print(f"{len(success)} calls succeeded")
+    success_paths = set(goal["path"] for goal in success)
+    failed_or_timed_out_paths = set(goal["path"] for goal in failed + timed_out) - success_paths
+    print(f"{len(failed_or_timed_out_paths)} goals failed or timed out (all calls)")
+    print(f"{len(success_paths)} goals succeeded")
     jeha_fails_or_timeouts = [goal["path"] for goal in failed + timed_out if goal["method"] == "jeha"]
+    jeha_success = [goal["path"] for goal in success if goal["method"] == "jeha"]
+    # print(jeha_success[0])
     # print(jeha_fails)
     metis_grouped = {}
     for goal in results:
@@ -56,20 +62,28 @@ def analyse_file(filename):
             else:
                 metis_grouped[path] = [goal]
     metis_all_fails_or_timeouts = [path for path, goals in metis_grouped.items() if all(not is_success(goal) for goal in goals)]
+    metis_any_success = [path for path, goals in metis_grouped.items() if any(is_success(goal) for goal in goals)]
+    # print(metis_any_success[0])
     print(f"jeha fails or timeouts: {len(jeha_fails_or_timeouts)}")
+    print(f"jeha success: {len(jeha_success)}")
     print(f"metis fails (no variant worked): {len(metis_all_fails_or_timeouts)}")
-    jeha_success_metis_fail_or_timeout = set(metis_all_fails_or_timeouts) - set(jeha_fails_or_timeouts)
+    print(f"metis success (any variant workd): {len(metis_any_success)}")
+    # jeha_success_metis_fail_or_timeout = set(metis_all_fails_or_timeouts) - set(jeha_fails_or_timeouts)
+    jeha_success_metis_fail_or_timeout = set(jeha_success) - set(metis_any_success)
     print(f"jeha success, metis fail: {str(len(jeha_success_metis_fail_or_timeout))}")
     print(jeha_success_metis_fail_or_timeout)
+    # print(sorted(list(jeha_success_metis_fail_or_timeout))[:10])
     print(f"metis success, jeha fail: {str(len(set(jeha_fails_or_timeouts) - set(metis_all_fails_or_timeouts)))}")
-    
+
 if __name__ == "__main__":
     if not os.path.isdir("runs"):
         raise RuntimeError("Couldn't find directory with name 'runs'.")
-    runs_dirs = os.listdir("runs")
+    runs_dirs = sorted(list(os.listdir("runs")))
     for dirname in runs_dirs:
         filename = "runs/" + dirname + "/mirabelle.log"
-        print(filename)
+        with open("runs/" + dirname + "/commit") as c:
+            commit = c.read()
+        print(filename, commit)
         try:
             analyse_file(filename)
         except FileNotFoundError:
