d4beb17212a7f1ea425cff1335f421daf469216a
Massin Guerdi <massin.guerdi@ifi.lmu.de>
2025-06-29 21:29:25 +0200

    start introducing new active set

--- jeha_base.thy
+++ jeha_base.thy
@@ -59,6 +59,7 @@ ML_file \<open>jeha_unify.ML\<close>
 ML_file \<open>jeha_subsumption.ML\<close>
 ML_file \<open>jeha_simplify.ML\<close>
 ML_file \<open>jeha_passive.ML\<close>
+ML_file \<open>jeha_clause_set.ML\<close>
 ML_file \<open>jeha.ML\<close>
 
 end
