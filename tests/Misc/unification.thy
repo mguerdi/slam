75ecbb453264ae44b71016bb5ef362732efeffcc
Massin Guerdi <massin.guerdi@ifi.lmu.de>
2025-08-19 15:59:45 +0200

    fix maxidxforcer
    fixes tests/Misc/unification.thy:36

--- tests/Misc/unification.thy
+++ tests/Misc/unification.thy
@@ -36,6 +36,48 @@ ML_val \<open>
   \<^assert> (maxidx <= Envir.maxidx_of right_matcher);
 \<close>
 
+(* Notes on Unify.matchers
+
+  (* in Jeha_Unify.matchers *)
+  val v = Var (("maxidxforcer", maxidx), dummyT)
+  val pairs = map (apply2 Jeha_Unify.give_to_undefined) ((v, Term.dummy) :: [(pattern, term)])
+
+  (* in Unify.matchers *)
+  val context = (Context.Proof @{context})
+  val thy = Context.theory_of context;
+
+  (* maxidx of the term to be matched *)
+  val maxidx = fold (Term.maxidx_term o #2) pairs ~1;
+  val offset = maxidx + 1;
+  (* rename patterns above terms *)
+  val pairs' = map (apfst (Logic.incr_indexes ([], offset))) pairs;
+  (* resulting overall maxidx *)
+  val maxidx' = fold (fn (t, u) => Term.maxidx_term t #> Term.maxidx_term u) pairs' ~1;
+
+  (* this applies to the result of Unify.smash_unifiers *)
+
+  val decr_indexesT_same =
+    Term.map_atyps_same
+      (fn TVar ((x, i), S) =>
+          (* *)
+          if i > maxidx then TVar ((x, i - offset), S) else raise Same.SAME
+        | _ => raise Same.SAME);
+  val decr_indexesT = Same.commit decr_indexesT_same;
+  val decr_indexes =
+    Term.map_types decr_indexesT_same #>
+    Term.map_aterms
+      (fn Var ((x, i), T) =>
+          if i > maxidx then Var ((x, i - offset), T) else raise Same.SAME
+        | _ => raise Same.SAME);
+
+  (* The check
+    Envir.above maxidx
+  tests, if
+  *)
+
+  val empty = Envir.empty maxidx';
+*)
+
 ML_val \<open>
   val x = @{term_schem "?x :: ?'a"};
   val y = @{term_schem "?y :: ?'b"};
