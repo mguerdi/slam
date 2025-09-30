c8ea8e6740a6636e3ecf08e2ad2716746a1cb030
Massin Guerdi <massin.guerdi@ifi.lmu.de>
2025-09-30 22:04:13 +0200

    fix dd reconstruction

--- tests/Reconstruction/dd.thy
+++ tests/Reconstruction/dd.thy
@@ -10,6 +10,21 @@ ML \<open>
     Thm.eq_thm_prop (th1, th2)
 \<close>
 
+(*
+(* If every predicate that distinguishes c and d is constant, then c and d must be equal *)
+lemma "(\<And>(P :: 'a \<Rightarrow> bool). P c \<noteq> P d \<Longrightarrow> \<exists>y. \<forall>x. y = P x) \<Longrightarrow> c = d"
+  using [[slam_trace]] by slam (* this had a reconstruction bug *)
+*)
+
+ML_val \<open>
+  val C = mk @{term_schem "?P c \<noteq> ?P d \<Longrightarrow> ?P c \<noteq> ?P d \<Longrightarrow> ?P ?x3 \<noteq> ?P ?x1 \<Longrightarrow> False"}
+  val expected = mk @{term_schem "?P c \<noteq> ?P d \<Longrightarrow> ?P ?x3 \<noteq> ?P ?x1 \<Longrightarrow> False"}
+  val conclusion =
+    Slam_Proof.reconstruct_delete_duplicated_lits
+      { premise = C, duplicate_cposs = [{duplicate = 1, duplicate_of = 0, orientation = JLit.Left}] }
+  val () = \<^assert> (eq_thm (expected, conclusion))
+\<close>
+
 ML_val \<open>
   val C = mk @{prop "A \<Longrightarrow> A \<Longrightarrow> B \<Longrightarrow> False"}
   val expected = mk @{prop "A \<Longrightarrow> B \<Longrightarrow> False"}
