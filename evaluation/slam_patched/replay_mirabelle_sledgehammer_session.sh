f268a613a9d5567e9c8b219c4f212faf75f531ea
Massin Guerdi <massin.guerdi@ifi.lmu.de>
2025-11-30 16:02:13 +0100

    rework how evaluation containers are run

---
+++ evaluation/slam_patched/replay_mirabelle_sledgehammer_session.sh
@@ -0,0 +1,4 @@
+# runs in the container
+mkdir -p "$HOME/mirabelle_output/$1"
+cat "$HOME/mirabelle_output/mirabelle-long-run.log" > "$HOME/mirabelle_output/$1/mirabelle.log"
+"$HOME/Isabelle/bin/isabelle" mirabelle -o "threads=1" -g 1 -O "$HOME/mirabelle_output/$1" -A 'sledgehammer_replay[provers=zipperposition, mini_preplay_inputs=true, proof_methods="metis slam", instantiate=false, preplay_timeout=2, smt_proofs=false]' -X slow "$1"
