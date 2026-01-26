d458a4e162fcd9a8244ca9e8c14d9b4f5fc83130
Massin Guerdi <massin.guerdi@ifi.lmu.de>
2026-01-26 17:59:24 +0100

    FlexFlexSimp

---
+++ tests/Reconstruction/flex_flex_simp.thy
@@ -0,0 +1,55 @@
+theory flex_flex_simp
+
+imports SLAM_TEST_BASE.test_base
+
+begin
+
+ML \<open>
+  val empty = mkh @{prop "False"}
+\<close>
+
+ML_val \<open>
+  val c = mkh @{term_schem "?a = ?b \<Longrightarrow> False"}
+  val c' = Slam_Proof.reconstruct_flex_flex_simp @{context} { premise = c }
+\<close>
+
+ML_val \<open>
+  val c = mkh @{term_schem "?a c = ?b d \<Longrightarrow> False"}
+  val c' = Slam_Proof.reconstruct_flex_flex_simp @{context} { premise = c }
+  val () = \<^assert> (eqh (c', empty))
+\<close>
+
+ML_val \<open>
+  val c = mkh @{term_schem "?a = ?b \<Longrightarrow> ?a = ?b \<Longrightarrow> False"}
+  val c' = Slam_Proof.reconstruct_flex_flex_simp @{context} { premise = c }
+  val () = \<^assert> (eqh (c', empty))
+\<close>
+
+ML_val \<open>
+  val c = mkh @{term_schem "?a = ?b \<Longrightarrow> ?a = ?c \<Longrightarrow> False"}
+  val c' = Slam_Proof.reconstruct_flex_flex_simp @{context} { premise = c }
+  val () = \<^assert> (eqh (c', empty))
+\<close>
+
+ML_val \<open>
+  val c = mkh @{term_schem "?a a = ?b b \<Longrightarrow> ?a c = ?c d \<Longrightarrow> False"}
+  val c' = Slam_Proof.reconstruct_flex_flex_simp @{context} { premise = c }
+  val () = \<^assert> (eqh (c', empty))
+\<close>
+
+ML_val \<open>
+  val c = mkh @{term_schem "?a a b = ?a b c \<Longrightarrow> ?a (d ?a) = ?a e \<Longrightarrow> False"}
+  val c' = Slam_Proof.reconstruct_flex_flex_simp @{context} { premise = c }
+  val () = \<^assert> (eqh (c', empty))
+\<close>
+
+ML_val \<open>
+  val c: HClause.hthm =
+    { th = mk @{term_schem "sk = sk_def \<Longrightarrow> ?a = ?b \<Longrightarrow> False"}
+    , skolems = [(@{term "sk"}, @{term "sk_def"})]
+    }
+  val c' = Slam_Proof.reconstruct_flex_flex_simp @{context} { premise = c }
+  val () = \<^assert> (eqh (c', empty))
+\<close>
+
+end
