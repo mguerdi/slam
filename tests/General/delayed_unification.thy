36318027da10135abca5c90b815d45b2bec905ee
Massin Guerdi <massin.guerdi@ifi.lmu.de>
2026-04-19 19:56:00 +0200

    document SIMPL-only idea

--- tests/General/delayed_unification.thy
+++ tests/General/delayed_unification.thy
@@ -112,6 +112,41 @@ ML_val \<open>
 *)
 \<close>
 
+(* Possible new unification variant: SIMPL-only *)
+(* Reconstruction idea:
+SIMPL stops at ?x a = g a
+unification would continue with projection, imitation
+But what if search_bound is 0?
+ *)
+
+ML_val\<open>
+  val s_neq_t = @{term_schem "(h::'c \<Rightarrow> 'c \<Rightarrow> 'd) ((?y::'b \<Rightarrow> 'c) (b::'b)) (?y (a::'b)) \<noteq> h ((g::'a \<Rightarrow> 'c) ((f::'b \<Rightarrow> 'a) b)) (g (c::'a))"}
+  val (s, t) = HOLogic.dest_eq (HOLogic.dest_not s_neq_t)
+  val (pretty_s, pretty_t) = apply2 (Thm.cterm_of @{context}) (s, t)
+  val (env, ff, fr) =
+    Slam_Isabelle_Unify.SIMPL
+      (Context.Proof @{context})
+      (Envir.empty 10, [([], s, t)])
+  fun to_ct_pair ([], u, v) = apply2 (Thm.cterm_of @{context}) (u, v)
+  val frct = map to_ct_pair fr
+\<close>
+
+(* This does not help. Can e-unification help? I.e. is it possible to build a SIMPL-only unifier
+there? *)
+declare [[slam_unify_search_bound=0]]
+ML_val\<open>
+  val s_neq_t = @{term_schem "(h::'c \<Rightarrow> 'c \<Rightarrow> 'd) ((?y::'b \<Rightarrow> 'c) (b::'b)) (?y (a::'b)) \<noteq> h ((g::'a \<Rightarrow> 'c) ((f::'b \<Rightarrow> 'a) b)) (g (c::'a))"}
+  val (s, t) = HOLogic.dest_eq (HOLogic.dest_not s_neq_t)
+  val (pretty_s, pretty_t) = apply2 (Thm.cterm_of @{context}) (s, t)
+  val unifiers =
+    Slam_Unify.preunifiers
+      (Context.Proof @{context})
+      [(s, t)]
+      (Envir.empty 10)
+  val u = Seq.pull unifiers
+\<close>
+
+
 declare [[slam_supress_unify_trace=true]]
 declare [[slam_isabelle_unify_trace=false]]
 declare [[slam_isabelle_unify_trace_bound=60]]
