da3a9e2d40e8161fa99385fede8fea5cfde03306
Massin Guerdi <massin.guerdi@ifi.lmu.de>
2026-06-01 22:57:43 +0200

    fix readonly option unknown to podman

--- evaluation/tptp/run_tptp.sh
+++ evaluation/tptp/run_tptp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-podman run --userns keep-id:uid=1000,gid=1000 -v ~/TPTPv9.0.0:/home/isabelle/TPTPv9.0.0:readonly mguerdi/isabelle-slam-tptp ./git/evaluation/tptp/rating_zero_problems
+podman run --userns keep-id:uid=1000,gid=1000 -v ~/TPTPv9.0.0:/home/isabelle/TPTPv9.0.0:ro mguerdi/isabelle-slam-tptp ./git/evaluation/tptp/rating_zero_problems
