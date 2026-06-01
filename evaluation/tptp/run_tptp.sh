f9f3355e5761b9fa0cd78196886142a39414be02
Massin Guerdi <massin.guerdi@ifi.lmu.de>
2026-06-01 23:10:57 +0200

    fix TPTP runner script

--- evaluation/tptp/run_tptp.sh
+++ evaluation/tptp/run_tptp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-podman run --userns keep-id:uid=1000,gid=1000 -v ~/TPTP-v9.0.0:/home/isabelle/TPTP-v9.0.0:ro mguerdi/isabelle-slam-tptp ./git/evaluation/tptp/rating_zero_problems
+podman run --userns keep-id:uid=1000,gid=1000 -v ~/TPTP-v9.0.0:/home/isabelle/TPTP-v9.0.0:ro mguerdi/isabelle-slam-tptp slam_tptp ./git/evaluation/tptp/rating_zero_problems
