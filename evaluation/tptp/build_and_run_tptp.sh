1f63ce69368eed277d5b9f5545c3a924c159c7d5
Massin Guerdi <massin.guerdi@ifi.lmu.de>
2026-06-01 22:44:01 +0200

    fix tptp build script

--- evaluation/tptp/build_and_run_tptp.sh
+++ evaluation/tptp/build_and_run_tptp.sh
@@ -4,6 +4,6 @@ if [[ "$PWD" != "$(git rev-parse --show-toplevel)" ]]; then
   echo Run from top level of slam git repository. Exiting.
   exit 1
 fi
-podman build --format=docker --tag="mguerdi/isabelle-slam-tptp" --build-context slam-repo=. --file="evaluation/slam_tptp/Dockerfile"
+podman build --format=docker --tag="mguerdi/isabelle-slam-tptp" --build-context slam-repo=. --file="evaluation/tptp/Dockerfile"
 ./evaluation/tptp_slam/run_tptp.sh
 echo DONE
