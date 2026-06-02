8a54fcd88fd6f1a3a4d5a4992c2a11dab012412e
Massin Guerdi <massin.guerdi@ifi.lmu.de>
2026-06-02 12:02:00 +0200

    correct Isabelle version for TPTP
    AFP evaluation version doesn't have process_theories

---
+++ evaluation/tptp/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -euo pipefail
+if [[ "$PWD" != "$(git rev-parse --show-toplevel)" ]]; then
+  echo Run from top level of slam git repository. Exiting.
+  exit 1
+fi
+podman build --format=docker --tag="mguerdi/isabelle-slam-tptp" --build-context slam-repo=. --file="evaluation/tptp/Dockerfile"
--- evaluation/tptp/build_and_run_tptp.sh
+++ evaluation/tptp/build_and_run_tptp.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-set -euo pipefail
-if [[ "$PWD" != "$(git rev-parse --show-toplevel)" ]]; then
-  echo Run from top level of slam git repository. Exiting.
-  exit 1
-fi
-podman build --format=docker --tag="mguerdi/isabelle-slam-tptp" --build-context slam-repo=. --file="evaluation/tptp/Dockerfile"
+./build.sh
 ./evaluation/tptp/run_tptp.sh
-echo DONE
