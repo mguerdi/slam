68562c71f77bf0b1354cd6c006b5ad89be63d866
Massin Guerdi <massin.guerdi@ifi.lmu.de>
2026-06-02 12:17:18 +0200

    fix tptp scripts

--- evaluation/tptp/build_and_run_tptp.sh
+++ evaluation/tptp/build_and_run_tptp.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-./build.sh
+set -euo pipefail
+if [[ "$PWD" != "$(git rev-parse --show-toplevel)" ]]; then
+  echo Run from top level of slam git repository. Exiting.
+  exit 1
+fi
+./evaluation/tptp/build.sh
 ./evaluation/tptp/run_tptp.sh
