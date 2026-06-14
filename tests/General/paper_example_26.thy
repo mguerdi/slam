ef480faa9b960e2d9fac40ff84d3854347b001c6
Massin Guerdi <massin.guerdi@ifi.lmu.de>
2026-06-15 00:11:24 +0200

    WIP smart options

--- tests/General/paper_example_26.thy
+++ tests/General/paper_example_26.thy
@@ -8,17 +8,17 @@ lemma paper_example_26_all_rules:
   shows "(\<exists> y. \<forall> x . y x = (p x \<and> q x))"
   (* sledgehammer suggests only: *)
   (* by moura (* 5 ms *) *)
-  by (slam) (* 34 ms *)
+  by slam (* 34 ms *)
   (* using [[slam_meson, slam_max_number_of_steps=1000]] by (slam) (* doesn't work *) *)
 
 (* Faithful reproduction of the proof of example 26 in the paper. *)
 
 declare [[slam_disable_all]]
 
-declare [[slam_rule_forall_rw]]
-declare [[slam_rule_exists_hoist]]
-declare [[slam_rule_bool_rw]]
-declare [[slam_rule_simp_false_elim]]
+declare [[slam_rule_forall_rw=on]]
+declare [[slam_rule_exists_hoist=on]]
+declare [[slam_rule_bool_rw=on]]
+declare [[slam_rule_simp_false_elim=on]]
 
 lemma paper_example_26_restricted:
   shows "(\<exists> y. \<forall> x . y x = (p x \<and> q x))"
